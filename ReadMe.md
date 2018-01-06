be3177305a2b01e1c7641fbee8ab743b4896402e
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-01-05 19:49:48 -0500

    Adoption of Throttle Limit Action Groups Renamed to Throttle Limit Extended Recompiled for 1.3.1 Added ThrottleLimiterModule

---
+++ ReadMe.md
@@ -0,0 +1,12 @@
+Originial thread is here:
+
+https://forum.kerbalspaceprogram.com/index.php?/topic/108617-10x-discontinued-throttle-limit-action-groups-tweak-throttle-limits-of-engines-via-action-groups-may-18/
+
+Originally suggested by Kofeyh of KSPTV, this super simple mod allows players to assign levels of throttle limiting to an engine on their craft through the magical power of action groups.
+
+These action groups work like any of the stock ones, meaning they will also copy to symmetry counterparts.
+
+Note that users of AGX will have to abide by AGX's cooldown periods and cannot simply spam a button. Plan your groups accordingly.
+
+New Feature:  ThrottleLimiter
+	This is a new part module which provides a minimum throttle setting, so that the throttle will never go below that (unless it's turned off)
---
+++ Source/ThrottleLimiter.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KSP___ActionGroupEngines.Main
+{
+
+    public class ThrottleLimiterModule : PartModule
+    {
+        enum SetThrustValues { none, off, decrease, increase, auto };
+
+        [KSPField]
+        public float minThrottle = 0;
+
+        public override string GetModuleDisplayName()
+        {
+            return "Throttle Limit";
+        }
+        public override string GetInfo()
+        {
+            return "Minimum throttle: " + (100f * minThrottle).ToString("n0") + "%";         
+        }
+
+#if DEBUG
+        void Start()
+        {
+            Debug.Log("ThrottleLimiterModule:  part; " + part.partInfo.title + ",   minThrottle: " + minThrottle);
+        }
+#endif
+
+
+        //void CheckThrust()
+        void SetMinThrust(SetThrustValues stv)
+        {
+            Debug.Log("ThrottleLimiterModule.CheckThrust:  part; " + part.partInfo.title + ",   stv: " + stv.ToString());
+
+            foreach (PartModule m in this.part.Modules)
+                if (m is ModuleEngines)
+                {
+                    ModuleEngines engine = m as ModuleEngines;
+                    if (engine == null || !engine.isOperational || engine.engineType == EngineType.SolidBooster || engine.throttleLocked)
+                        continue;
+
+                    if (stv != SetThrustValues.off)
+                    {
+                        if (stv == SetThrustValues.decrease && engine.thrustPercentage <= 0.0f)
+                            continue;
+
+                        engine.throttleMin = minThrottle;
+                    }
+                    else
+                        engine.throttleMin = 0;
+                }
+                else if (m is ModuleEnginesFX && m.isEnabled)
+                {
+                    ModuleEnginesFX engineFx = m as ModuleEnginesFX;
+                    if (engineFx == null || !engineFx.isOperational || engineFx.engineType == EngineType.SolidBooster || engineFx.throttleLocked)
+                        continue;
+
+                    if (stv != SetThrustValues.off)
+                    {
+                        if (stv == SetThrustValues.decrease && engineFx.thrustPercentage <- 0.0f)
+                            continue;
+                        engineFx.throttleMin = minThrottle;
+                    }
+                    else
+                        engineFx.throttleMin = 0;
+                }
+        }
+
+
+        void Update()
+        {
+
+            if (GameSettings.THROTTLE_UP.GetKey() ||
+                 (Input.GetKey(KeyCode.LeftAlt) &&  GameSettings.WHEEL_THROTTLE_UP.GetKey())
+                 )
+                SetMinThrust(SetThrustValues.increase);
+
+
+            if (GameSettings.THROTTLE_DOWN.GetKey() ||
+                 (Input.GetKey(KeyCode.LeftAlt) && GameSettings.WHEEL_THROTTLE_DOWN.GetKey())
+                )
+                SetMinThrust(SetThrustValues.decrease);
+
+            if (GameSettings.THROTTLE_CUTOFF.GetKey())
+                SetMinThrust(SetThrustValues.off);
+
+            if (GameSettings.THROTTLE_FULL.GetKey())
+                SetMinThrust(SetThrustValues.increase);
+
+
+        }
+    }
+}
